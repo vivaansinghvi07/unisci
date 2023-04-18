569bc40a69502118d75884109da517e7fac85c53
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-18 10:39:37 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='uniconvert',
+    version='0.1',
+    author='Vivaan Singhvi',
+    author_email='singhvi.vivaan@gmail.com',
+    description='Units and Conversions Package',
+    license='MIT',
+    packages=['uniconvert'],
+    include_package_data=True
+)
