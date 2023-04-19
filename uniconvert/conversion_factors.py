4b84b5848081e58c2dce4c054b453d6550bcda8b
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-19 17:35:09 -0400

    added volume conversion factors

--- uniconvert/conversion_factors.py
+++ uniconvert/conversion_factors.py
@@ -29,6 +29,7 @@ CONVERT_TO_KILOGRAMS = {
     'mcg': 1e-9,
     'ng': 1e-12,
     'lb': 0.454,
+    'oz': 0.454 / 16,
     'st': 6.356,
     'amu': 1.661e-27,
     'u': 1.661e-27
@@ -50,3 +51,23 @@ CONVERT_TO_SECONDS = {
 }
 CONVERT_FROM_SECONDS = {key: 1 / value for key, value in CONVERT_TO_SECONDS.items()}
 TIME_UNITS = list(CONVERT_FROM_SECONDS.keys())
+
+# for volume conversions
+CONVERT_TO_LITERS = {
+    'nL': 1e-9,
+    'mcL': 1e-6,
+    'mL': 1e-3,
+    'L': 1,
+    'kL': 1e3,
+    'ML': 1e6,
+    'GL': 1e9,
+    'c.': 0.2366,
+    'pt.': 0.2366 * 2,
+    'qt.': 0.2366 * 4,
+    'gal.': 0.2366 * 16,
+    'fl. oz': 0.2366 / 8,
+    'tbsp': 0.2366 / 16,
+    'tsp': 0.2366 / 48
+}
+CONVERT_FROM_LITERS = {key: 1 / value for key, value in CONVERT_TO_LITERS.items()}
+VOLUME_UNITS = list(CONVERT_FROM_LITERS.keys())
