f9eabc0edea16594f7841c92d9a9995c970475c1
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-20 15:08:28 -0400

    added new error tpye

--- uniconvert/error.py
+++ uniconvert/error.py
@@ -2,3 +2,7 @@
 class UnitError(Exception):
     def __init__(self, message):
         self.message = message
+
+class CompatabilityError(Exception):
+    def __init__(self, message):
+        self.message = message
