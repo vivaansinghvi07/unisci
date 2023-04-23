4857b53eaab71b9f340b1ad1d66524d9aefe355d
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-23 01:13:11 -0400

    added error comment

--- uniconvert/error.py
+++ uniconvert/error.py
@@ -3,14 +3,17 @@ class UnitError(Exception):
     def __init__(self, message):
         self.message = message
 
+# for incompatible units and/or Quantities
 class CompatabilityError(Exception):
     def __init__(self, message):
         self.message = message
     
+# for operations not supported
 class UnsupportedError(Exception):
     def __init__(self, message):
         self.message = message
 
+# for arguments being entered incorrectly
 class ArgumentError(Exception):
     def __init__(self, message):
         self.message = message
