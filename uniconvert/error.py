6955fae72455a09830aacadc791e54fc166a0c2d
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-24 00:05:24 -0400

    moved error comments inside classes

--- uniconvert/error.py
+++ uniconvert/error.py
@@ -1,19 +1,27 @@
-# error message for invalid quantities
 class UnitError(Exception):
+    """
+    for invalid quantities
+    """
     def __init__(self, message):
         self.message = message
 
-# for incompatible units and/or Quantities
 class CompatabilityError(Exception):
+    """
+    for incompatible units and/or Quantities
+    """
     def __init__(self, message):
         self.message = message
     
-# for operations not supported
 class UnsupportedError(Exception):
+    """
+    for operations not supported
+    """
     def __init__(self, message):
         self.message = message
 
-# for arguments being entered incorrectly
 class ArgumentError(Exception):
+    """
+    for arguments being entered incorrectly
+    """
     def __init__(self, message):
         self.message = message
