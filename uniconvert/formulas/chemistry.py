22d3907d6d0d322134483e0eccb98561bbec44a0
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-25 17:56:46 -0400

    added acid and base calculations

--- uniconvert/formulas/chemistry.py
+++ uniconvert/formulas/chemistry.py
@@ -1,4 +1,4 @@
-from sympy import Eq, symbols, solve, log
+from sympy import Eq, symbols, solve, log, sqrt
 from typing import Union
 from uniconvert.types import *
 from uniconvert.types import numeric
@@ -67,7 +67,12 @@ def nernst_equation(reduction_potential: Union[numeric, Quantity] = None,
                     reaction_quotient: numeric = None,
                     electron_count: numeric = None) -> Quantity:
     """
-    Arguments: Four out of the following five: reduction potential, standard potential, temperature, reaction quotient, and electron count.
+    Arguments: Four out of the following five: 
+    - reduction_potential (V)
+    - standard_potential (V)
+    - temperature (K)
+    - reaction_quotient
+    - electron_count
 
     Raises: ArgumentError for too many or too little known values.
 
@@ -108,7 +113,11 @@ def buffer_system(K_a: Union[numeric, Quantity] = None,
                  pH: numeric = None):
     """
     Note: This function assumes dissassociation into one proton only.
-    Arguments: four of the following: the K_a value of the acid, the intital concentration (if in number form, assumed to be in M) of the acid, conjugate base, and protons, and the pH of the resulting solution.
+    Arguments: four of the following, interpreted to the following units: 
+    - K_a
+    - pH
+    - acid_concentration (M)
+    - base_concentration (M)
 
     Raises: ArgumentError for wrong argument count. One must be empty. A CompatabilityError for incompatible values.
 
@@ -132,6 +141,76 @@ def buffer_system(K_a: Union[numeric, Quantity] = None,
     args = _get_args(types=types, arguments=arguments)
 
     equation = Eq(args["pH"], -log(args["ka"], 10) + log(args["initial_conc_base"] / args["initial_conc_acid"], 10))
+
+    solution = solve(equation, (symbols(UNKNOWN)))
+
+    return solution[0]
+
+def weak_acid(K_a: Union[numeric, Quantity] = None,
+              acid_concentration: Union[numeric, Quantity] = None,
+              pH: numeric = None):
+    """
+    Note: This function assumes dissassociation into one proton only.
+    Arguments: two of the following, in the following units:
+    - K_a
+    - pH
+    - acid_concentration (M)
+
+    Raises: ArgumentError for wrong argument count. One must be empty. A CompatabilityError for incompatible values.
+
+    Returns: the missing value as a number.
+    """
+
+    arguments = {
+        "ka": K_a,
+        "initial_conc": acid_concentration,
+        "pH": pH
+    }
+
+    types = {
+        "ka": {},
+        "initial_conc": {'M': 1},
+        "pH": {}
+    }
+
+    args = _get_args(types=types, arguments=arguments)
+
+    equation = Eq(args['ka'], ((10**(-args['pH']))**2/(args['initial_conc'] - 10**(-args['pH']))))
+
+    solution = solve(equation, (symbols(UNKNOWN)))
+
+    return solution[0]
+
+def weak_base(K_b: Union[numeric, Quantity] = None,
+                 base_concentration: Union[numeric, Quantity] = None,
+                 pH: numeric = None):
+    """
+    Note: This function assumes dissassociation into one proton only, and no initial concentrations of anything else.
+    Arguments: two of the following, in the given units: 
+    - K_b
+    - pH
+    - base_concentration (M)
+
+    Raises: ArgumentError for wrong argument count. One must be empty. A CompatabilityError for incompatible values.
+
+    Returns: the missing value as a number.
+    """
+
+    arguments = {
+        "kb": K_b,
+        "initial_conc": base_concentration,
+        "pH": pH
+    }
+
+    types = {
+        "kb": {},
+        "initial_conc": {'M': 1},
+        "pH": {}
+    }
+
+    args = _get_args(types=types, arguments=arguments)
+
+    equation = Eq(args['kb'], ((10**(-(14-args['pH'])))**2/(args['initial_conc'] - 10**(-(14-args['pH'])))))
 
     solution = solve(equation, (symbols(UNKNOWN)))
 
