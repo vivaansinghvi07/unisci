bf785c4317f46f40cbf14db57f4d1c94a8afcbaf
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-24 23:28:24 -0400

    added some constants

--- uniconvert/constants.py
+++ uniconvert/constants.py
@@ -1,3 +1,8 @@
+from uniconvert.types import Quantity
+
 E = 2.718281828
 PI = 3.1415926536
-C = SPEED_OF_LIGHT = 299_792_458
+C = SPEED_OF_LIGHT = Quantity(299_792_458, {'m': 1, 's': -1})
+F = FARADAYS_CONSTANT = Quantity(96_485.332_123, {'C': 1, 'mol': -1})
+R = Quantity(8.3145, {'L': 1, 'atm': 1, 'mol': -1, 'K': -1})
+AVOGADRO_CONSTANT = Quantity(6.022_140e23, {'u': 1, 'mol': -1})
