ba85d3755b1cbcc2e563735b2c653ca56801ff7c
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-26 10:54:15 -0400

    added two kinds of R

--- uniconvert/constants.py
+++ uniconvert/constants.py
@@ -4,5 +4,6 @@ E = 2.718281828
 PI = 3.1415926536
 C = SPEED_OF_LIGHT = Quantity(299_792_458, {'m': 1, 's': -1})
 F = FARADAYS_CONSTANT = Quantity(96_485.332_123, {'C': 1, 'mol': -1})
-R = Quantity(8.3145, {'L': 1, 'atm': 1, 'mol': -1, 'K': -1})
+R_JOUL = Quantity(8.3145, {'J': 1, 'mol': -1, 'K': -1})
+R_ATM = Quantity(0.08206, {'L': 1, 'atm': 1, 'mol': -1, 'K': -1})
 AVOGADRO_CONSTANT = Quantity(6.022_140e23, {'u': 1, 'mol': -1})
