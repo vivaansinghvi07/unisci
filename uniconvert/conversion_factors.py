de7ee8dbf4c65542e505b5cb0c3b310bdca95093
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-26 00:20:32 -0400

    removed auto-simplify - no need with to_base

--- uniconvert/conversion_factors.py
+++ uniconvert/conversion_factors.py
@@ -1,29 +1,3 @@
-# what you need to multiply each of these by to get the base metric
-METRIC_CONVERSIONS = {
-    'y': 1e-24,
-    'z': 1e-21,
-    'a': 1e-18,
-    'f': 1e-15,
-    'p': 1e-12,
-    'n': 1e-9,
-    'µ': 1e-6,
-    'mc': 1e-6,     # for those who cannot use the µ symbol
-    'm': 1e-3,
-    'c': 1e-2,
-    'd': 1e-1,
-    'da': 1e1,
-    'h': 1e2,
-    'k': 1e3,
-    'M': 1e6,
-    'G': 1e9,
-    'T': 1e12,
-    'P': 1e15,
-    'E': 1e18,
-    'Z': 1e21,
-    'Y': 1e24
-}
-METRIC_UNITS = ['s', 'm', 'g', 'K', 'mol', 'J', 'W', 'N']     # the ones that are currently supported
-
 # for length: convert between the units shown here
 CONVERT_TO_METERS = {
     'ft': 0.3048, 
@@ -74,27 +48,21 @@ CONVERT_TO_KELVIN = {
     'K': 1,
 }
 
-# supported units for auto-simplification - first one for each special unit must be in the most basic form
-AUTO_SIMPLIFY = [
-    [{'kg': 1, 'm': 1, 's': -2}, 'N'],      # Newtons
-    [{'J': 1, 'm': -1}, 'N'],
-    [{'kg': 1, 'm': 2, 's': -2}, 'J'],      # Joules
-    [{'N': 1, 'm': 1}, 'J'],             
-    [{'kg': 1, 'm': 2, 's': -3}, 'W'],      # Watts
-    [{'N': 1, 'm': 1, 's': -1}, 'W'],
-    [{'J': 1, 's': -1}, 'W'],          
-    [{'mol': 1, 'L': -1}, 'M'],             # Molarity
-    [{'dm': 3}, 'L'],                       # Liters
-    [{'cm': 3}, 'mL'],                      # Milliliters
-    [{'J': 1, 'C': -1}, 'V']                # Volts
-]
-FORCE_SIMPLIFY = {
+# for pressure conversions
+CONVERT_TO_ATM = {
+    'torr': 0.0013157,
+    'bar': 0.986923,
+    'Pa': 0.0000098,
+    'psi': 0.068046
+}
+
+SIMPLIFICATION = {
     'N': {'kg': 1, 'm': 1, 's': -2},        # Newtons
     'J': {'kg': 1, 'm': 2, 's': -2},        # Joules
     'W': {'kg': 1, 'm': 2, 's': -3},        # Watts
     'L': {'dm': 3},                         # Liters
     'mL': {'cm': 3},                        # Milliliters
     'M': {'mol': 1, 'L': -1},               # Molarity
+    'V': {'J': 1, 'C': -1},                 # Volts
+    'Pa': {'N': 1, 'm': -2}                 # Pressure (Pascals)
 }
-SPECIAL_UNITS = set([unit for _, unit in AUTO_SIMPLIFY])    # stores the special units (['N', 'J', ...])
-FORCED_SPECIAL_UNITS = list(FORCE_SIMPLIFY.keys())
