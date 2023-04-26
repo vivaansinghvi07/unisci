ddecd7c359178ea86daa3ca768ddbac7949e57fb
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-26 19:24:14 -0400

    build tools

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     description = fh.read()
 
 setup(
-    name='uniconvert',
+    name='unisci',
     version='1.0.0',
     author='Vivaan Singhvi',
     author_email='singhvi.vivaan@gmail.com',
@@ -12,7 +12,7 @@ setup(
     long_description=description,
     long_description_content_type="text/markdown",
     license='MIT',
-    packages=['unisci'],
+    packages=find_packages(),
     python_requires='>=3.8',
     include_package_data=True
 )
