db08343403440a1e47a45fd852dc3cda305b17b2
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-26 12:15:50 -0400

    change setup

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    description = fh.read()
+
 setup(
     name='uniconvert',
-    version='0.1',
+    version='1.0.0',
     author='Vivaan Singhvi',
     author_email='singhvi.vivaan@gmail.com',
-    description='Units and Conversions Package',
+    description='Units Conversions, and Science Package',
+    long_description=description,
+    long_description_content_type="text/markdown",
     license='MIT',
-    packages=['uniconvert'],
+    packages=['unisci'],
+    python_requires='>=3.8',
     include_package_data=True
 )
